2f66f738370b5970c82f67effe1772a1b0d7f9aa
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-08-16 23:30:14 +0530

    basic matching

---
+++ resources/js/Pages/Games/Show.vue
@@ -0,0 +1,8 @@
+<template>
+    <AuthenticatedLayout>
+        <h2>Testing!</h2>
+    </AuthenticatedLayout>
+</template>
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+</script>
