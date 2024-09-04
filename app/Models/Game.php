c9cfd408ba1e54d976b26a3b676fcc7c731ef7d3
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-09-04 23:28:06 +0530

    feat:prune older games than one hour

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -1,14 +1,31 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use App\Models\User;
+
+
+/**
+ * @property int $id
+ * @property int $player_one_id
+ * @property int $Player_two_id
+ * @property array $state
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User $playerOne
+ * @property-read User $playerTwo
+ */
 
 class Game extends Model
 {
-    use HasFactory;
+    use HasFactory,Prunable;
 
     protected $fillable = [
         'player_one_id',
@@ -32,4 +49,10 @@ class Game extends Model
     {
         return $this->belongsTo(User::class, 'player_two_id');
     }
+
+    public function prunable() : Builder
+    {
+        return static::where('created_at','<=',now()->subHour())->whereNull('state')->whereNull('player_two_id'); 
+    }
+
 }
---
+++ focus.md
@@ -0,0 +1,2 @@
+1) prune game model(we don't need endless games in the lobby);
+2) force finising the game if user has left the game in between.
