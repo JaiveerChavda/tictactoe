4635e721cdd75bae611e722a7ee11cbe34131e84
JaiveerChavda <jayveersinhchavda555@gmail.com>
2024-09-10 15:17:00 +0530

    chore : fix game policy - php stan errors

--- app/Policies/GamePolicy.php
+++ app/Policies/GamePolicy.php
@@ -7,64 +7,8 @@ use App\Models\User;
 
 class GamePolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Game $game): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        //
-    }
-
     public function join(User $user, Game $game): bool
     {
         return $game->playerOne->id !== $user->id && $game->playerTwo === null;
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Game $game): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Game $game): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Game $game): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Game $game): bool
-    {
-        //
     }
 }
